2c2014c762575bc0d9315de01959085a4ff7d6b3
CodeAnge1 <mikhailovmaksim777@gmail.com>
2023-01-17 13:31:17 +0300

    Add islands purchase and new island

--- game.py
+++ game.py
@@ -93,6 +93,7 @@ class Resource(pygame.sprite.Sprite):
             self.rect = pygame.rect.Rect(self.pos[0] * TILE_SIZE - 20, self.pos[1] * TILE_SIZE - 100, 64, 128)
         else:
             self.rect = self.image.get_rect(topleft=(self.pos[0] * TILE_SIZE, self.pos[1] * TILE_SIZE - 25))
+            self.rect = self.image.get_rect(topleft=(self.pos[0] * TILE_SIZE, self.pos[1] * TILE_SIZE - 25))
             self.rect = self.rect.inflate(-32, -20)
 
     def cut_sheet(self, sheet, columns, rows):
@@ -121,6 +122,7 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self, pos, groups, obstacles_sprites, sheets, columns, rows, x, y):
         super().__init__(groups)
+        self.money_count = 0
         self.frames_right_with_pickaxe = []
         self.frames_left_with_pickaxe = []
         self.frames_right_without_pickaxe = []
@@ -161,11 +163,10 @@ class Player(pygame.sprite.Sprite):
                     break
             else:
                 if (resource.rect.x < (mouse_pos[0] + game.level.visible_sprites.offset.x) < resource.rect.x + 64) and \
-                        (resource.rect.y < (
-                                mouse_pos[1] + game.level.visible_sprites.offset.y) < resource.rect.y + 128):
+                        (resource.rect.y < (mouse_pos[1] + game.level.visible_sprites.offset.y) < resource.rect.y + 64):
                     resource.hp -= 1
                     resource.get_damage()
-                break
+                    break
 
     def change_direction(self):
         keys = pygame.key.get_pressed()
@@ -246,6 +247,8 @@ class Tile(pygame.sprite.Sprite):
 
 class Level:
     def __init__(self):
+        self.money_img = load_image("game_menu/money_image.png")
+        self.money_rect = self.money_img.get_rect(topleft=(5, HEIGHT - 80))
         self.display_surface = pygame.display.get_surface()
         self.visible_sprites = CameraMovement()
         self.ground_sprites = pygame.sprite.Group()
@@ -273,6 +276,21 @@ class Level:
                                                                 load_image("animation/pickaxe_attack_right.png")], 7, 6,
                                            64, 64)
 
+    def add_new_space(self, level_name):
+        self.possible_positions.clear()
+        for i in self.obstacle_sprites:
+            if str(i) == "<Tile Sprite(in 1 groups)>":
+                self.obstacle_sprites.remove(i)
+        for row_index, row in enumerate(level_name):
+            for col_index, col in enumerate(row):
+                x, y = col_index * TILE_SIZE, row_index * TILE_SIZE
+                if col == ' ':
+                    self.possible_positions.append((row_index, col_index))
+                if col == 'x':
+                    Tile((x, y - TILE_SIZE // 2), [self.obstacle_sprites], None)
+        for i in self.impossible_positions:
+            self.possible_positions.remove(i)
+
     def run(self):
         self.visible_sprites.update()
         self.visible_sprites.draw(self.player)
@@ -281,6 +299,10 @@ class Level:
         if self.player.is_attack:
             self.pickaxe.update()
             self.pickaxe.draw(self.display_surface)
+        self.money_count_text = font.render(str(self.player.money_count), True, "white")
+        self.money_count_rect = self.money_count_text.get_rect(topleft=(75, HEIGHT - 53))
+        self.display_surface.blit(self.money_count_text, self.money_count_rect)
+        self.display_surface.blit(self.money_img, self.money_rect)
 
 
 class CameraMovement(pygame.sprite.Group):
@@ -291,10 +313,16 @@ class CameraMovement(pygame.sprite.Group):
 
         self.floor_surf = load_image("islands/first_island.png").convert()
         self.floor_rect = self.floor_surf.get_rect()
+        self.floor_surf_island_2 = load_image("islands/second_island.png").convert()
+        self.floor_rect_island_2 = self.floor_surf.get_rect(topleft=(-832, -768))
 
     def draw(self, player):
-        floor_offset_pos = self.floor_rect.topleft - self.offset
-        self.display_surface.blit(self.floor_surf, floor_offset_pos)
+        if game.buy.islands_count == 1:
+            floor_offset_pos = self.floor_rect.topleft - self.offset
+            self.display_surface.blit(self.floor_surf, floor_offset_pos)
+        if game.buy.islands_count == 2:
+            floor_offset_pos = self.floor_rect_island_2.topleft - self.offset
+            self.display_surface.blit(self.floor_surf_island_2, floor_offset_pos)
 
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             self.offset.x = player.rect.centerx - self.display_surface.get_width() // 2
@@ -304,45 +332,44 @@ class CameraMovement(pygame.sprite.Group):
             self.display_surface.blit(sprite.image, offset_position)
 
 
-class Forge:
-    def __init__(self):
-        pass
-
-
-class Furnace(pygame.sprite.Sprite):
-    def __init__(self, groups):
-        super().__init__(groups)
-        self.display_surface = pygame.display.get_surface()
-        self.image = load_image("buildings\Furnace.png")
-        self.pos = ()
-        self.rect = pygame.rect.Rect((-100, -100, 0, 0))
-
-    def build(self, pos):
-        if game.inventory.item_in_inventory("rock"):
-            if game.inventory.get_item_count("rock") >= 10:
-                game.inventory.remove_item("rock", 10)
-                game.level.possible_positions.remove((self.pos[1], self.pos[0]))
-                self.pos = pos
-                self.rect = pygame.rect.Rect(self.pos[0] * TILE_SIZE, self.pos[1] * TILE_SIZE, 45, 45)
-                self.rect.inflate(20, 10)
-
-    def draw(self):
-        self.display_surface.blit(self.image, self.rect)
-
-
-class BuildMenu:
-    def __init__(self):
-        self.display_surface = pygame.display.get_surface()
-        self.building_image = load_image("game_menu/inventory_image.png")
-        self.building_image_rect = self.building_image.get_rect(topleft=(WIDTH // 2 - 100, 5))
-
-    def draw(self, screen):
-        screen.blit(self.building_image, self.building_image_rect)
-
-
-class LandPurchase:
-    def __init__(self):
-        pass
+# class Forge:
+#     def __init__(self):
+#         pass
+#
+#
+# class Furnace(pygame.sprite.Sprite):
+#     def __init__(self, groups):
+#         super().__init__(groups)
+#         self.display_surface = pygame.display.get_surface()
+#         self.image = load_image("buildings\Furnace.png")
+#         self.pos = ()
+#         self.is_build = False
+#         self.rect = pygame.rect.Rect((-100, -100, 0, 0))
+#
+#     def build(self):
+#         if not self.is_build:
+#             if game.inventory.item_in_inventory("rock"):
+#                 if game.inventory.get_item_count("rock") >= 10:
+#                     game.inventory.remove_item("rock", 10)
+#                     self.is_build = True
+#                     pos = game.mouse_pos[0] // TILE_SIZE, game.mouse_pos[1] // TILE_SIZE
+#                     # game.level.possible_positions.remove((self.pos[1], self.pos[0]))
+#                     self.pos = pos
+#                     self.rect = pygame.rect.Rect(self.pos[0] * TILE_SIZE, self.pos[1] * TILE_SIZE, 45, 45)
+#                     self.rect.inflate(20, 10)
+#
+#     def draw(self):
+#         self.display_surface.blit(self.image, self.rect)
+#
+#
+# class BuildMenu:
+#     def __init__(self):
+#         self.display_surface = pygame.display.get_surface()
+#         self.building_image = load_image("game_menu/inventory_image.png")
+#         self.building_image_rect = self.building_image.get_rect(topleft=(WIDTH // 2 - 100, 5))
+#
+#     def draw(self, screen):
+#         screen.blit(self.building_image, self.building_image_rect)
 
 
 class Inventory:
@@ -358,7 +385,7 @@ class Inventory:
         self.background_image = load_image("game_menu/inventory_background.png")
         self.background_rect = self.background_image.get_rect(topleft=(WIDTH // 2 - 437, HEIGHT // 2 - 123))
         self.inventory_image = load_image("game_menu/inventory_image.png")
-        self.inventory_image_rect = self.inventory_image.get_rect(topleft=(WIDTH // 2 - 200, 5))
+        self.inventory_image_rect = self.inventory_image.get_rect(topleft=(WIDTH // 2 - 100, 5))
 
     def append_item(self, item_name, item_count):
         if item_name in self.items.keys():
@@ -379,7 +406,6 @@ class Inventory:
         return item in self.items.keys()
 
     def draw(self, screen):
-        screen.blit(self.inventory_image, self.inventory_image_rect)
         if game.inventory_menu:
             screen.blit(self.background_image, self.background_rect)
             self.offset = (104, 0)
@@ -405,10 +431,14 @@ class Game:
         self.game_is_paused = False
         self.inventory_menu = False
         self.land_purchase_menu = False
-        self.build_menu = False
+        self.buy_menu = False
+        self.sell_menu = False
         self.escape_count = 0
         self.inventory = Inventory()
-        self.build = BuildMenu()
+        self.in_game_menu = InGameMenu()
+        self.sell = ItemSell()
+        self.buy = LandPurchase()
+        # self.build = BuildMenu()
         pygame.init()
         size = WIDTH, HEIGHT
         self.screen = pygame.display.set_mode(size)
@@ -428,7 +458,7 @@ class Game:
         pygame.time.set_timer(self.resource_generate_event, 40000)
         self.level = Level()
         pos = random.choice(self.level.possible_positions)
-        self.furnace = Furnace([self.level.visible_sprites, self.level.obstacle_sprites])
+        # self.furnace = Furnace([self.level.visible_sprites, self.level.obstacle_sprites])
         # self.forge = Forge([self.level.visible_sprites, self.level.obstacle_sprites])
         for i in range(random.randint(3, 9)):
             self.resource_generate()
@@ -478,24 +508,26 @@ class Game:
                     if event.type == pygame.MOUSEMOTION:
                         self.mouse_pos = event.pos
                     if self.game_is_paused:
-                        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                            self.inventory_menu = False
-                            if (self.build.building_image_rect[0] < self.mouse_pos[0] < self.build.building_image_rect[
-                                0] + self.build.building_image_rect[2]) and \
-                                    (self.build.building_image_rect[1] < self.mouse_pos[1] <
-                                     self.build.building_image_rect[1] + self.build.building_image_rect[3]):
-                                self.build_menu = True
-                                self.inventory_menu = False
-                                self.land_purchase_menu = False
-                            if (self.inventory.inventory_image_rect[0] < self.mouse_pos[0] <
-                                self.inventory.inventory_image_rect[0] + self.inventory.inventory_image_rect[2]) and \
-                                    (self.inventory.inventory_image_rect[1] < self.mouse_pos[1] <
-                                     self.inventory.inventory_image_rect[1] + self.inventory.inventory_image_rect[3]):
-                                self.build_menu = False
-                                self.inventory_menu = True
-                                self.land_purchase_menu = False
+                        # if self.build_menu:
+                        #     # self.furnace.build()
+                        # # if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                        # #     self.inventory_menu = False
+                        #     # if (self.build.building_image_rect[0] < self.mouse_pos[0] < self.build.building_image_rect[
+                        #     #     0] + self.build.building_image_rect[2]) and \
+                        #     #         (self.build.building_image_rect[1] < self.mouse_pos[1] <
+                        #     #          self.build.building_image_rect[1] + self.build.building_image_rect[3]):
+                        #     #     self.build_menu = True
+                        #     #     self.inventory_menu = False
+                        #     #     self.land_purchase_menu = False
+                        #     if (self.inventory.inventory_image_rect[0] < self.mouse_pos[0] <
+                        #         self.inventory.inventory_image_rect[0] + self.inventory.inventory_image_rect[2]) and \
+                        #             (self.inventory.inventory_image_rect[1] < self.mouse_pos[1] <
+                        #              self.inventory.inventory_image_rect[1] + self.inventory.inventory_image_rect[3]):
+                        #         self.build_menu = False
+                        #         self.inventory_menu = True
+                        #         self.land_purchase_menu = False
                         self.inventory.draw(self.screen)
-                        self.build.draw(self.screen)
+                        # self.build.draw(self.screen)
                     else:
                         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                             self.is_up = False
@@ -504,6 +536,8 @@ class Game:
                                 self.level.player.attack(event.pos)
                             self.attack_count = 1
                             self.anim_is_end = False
+                            if self.game_is_paused:
+                                self.in_game_menu.update()
                         if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
                             self.is_up = True
                             self.attack_count = 0
@@ -511,10 +545,14 @@ class Game:
                         if event.type == self.resource_generate_event:
                             if self.level.possible_positions:
                                 self.resource_generate()
+
                 self.level.run()
                 if self.game_is_paused:
                     self.inventory.draw(self.screen)
-                    self.build.draw(self.screen)
+                    self.in_game_menu.draw(self.screen)
+                    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                        self.in_game_menu.update()
+                    # self.build.draw(self.screen)
                 self.clock.tick(60)
                 if self.level.pickaxe.frames_pickaxe_attack_left[-1] == self.level.pickaxe.image or \
                         self.level.pickaxe.frames_pickaxe_attack_right[-1] == self.level.pickaxe.image:
@@ -534,6 +572,72 @@ class Game:
                 f"player_pos = {self.level.player.get_pos()}, offset = {self.level.visible_sprites.offset}, mouse = {self.mouse_pos}")
             pygame.display.flip()
         pygame.quit()
+
+
+class ItemSell:
+    def __init__(self):
+        self.image = load_image("game_menu/sell_menu.png")
+        self.rect = self.image.get_rect(topleft=(WIDTH // 2 - 32, 5))
+        self.prices = {"bush": 3,
+                       "coal_ore": 10,
+                       "gold_ore": 35,
+                       "iron_ore": 25,
+                       "rock": 10,
+                       "tree": 7}
+
+    def sell_all(self):
+        for k, v in game.inventory.items.items():
+            game.inventory.items[k] = 0
+            game.level.player.money_count += v * self.prices[k]
+
+
+class LandPurchase:
+    def __init__(self):
+        self.image = load_image("game_menu/buy_menu.png")
+        self.rect = self.image.get_rect(topleft=(WIDTH // 2 + 36, 5))
+        self.islands_count = 1
+        self.second_island_is_buy = False
+
+    def buy_island(self):
+        if not self.second_island_is_buy:
+            if game.level.player.money_count >= 100:
+                game.level.player.money_count -= 100
+                self.islands_count = 2
+                game.level.add_new_space(MAP_LEVEL_2)
+                self.second_island_is_buy = True
+
+
+class InGameMenu:
+    def __init__(self):
+        pass
+
+    def is_inventory(self):
+        game.inventory_menu = True
+
+    def is_buy(self):
+        game.buy.buy_island()
+
+    def draw(self, screen):
+        screen.blit(game.inventory.inventory_image, game.inventory.inventory_image_rect)
+        screen.blit(game.sell.image, game.sell.rect)
+        screen.blit(game.buy.image, game.buy.rect)
+
+    def update(self):
+        if (game.inventory.inventory_image_rect[0] < game.mouse_pos[0] < game.inventory.inventory_image_rect[
+                0] + game.inventory.inventory_image_rect[2]) and \
+                    (game.inventory.inventory_image_rect[1] < game.mouse_pos[1] <
+                     game.inventory.inventory_image_rect[1] + game.inventory.inventory_image_rect[3]):
+            self.is_inventory()
+        if (game.sell.rect[0] < game.mouse_pos[0] < game.sell.rect[
+            0] + game.sell.rect[2]) and \
+                (game.sell.rect[1] < game.mouse_pos[1] <
+                 game.sell.rect[1] + game.sell.rect[3]):
+            game.sell.sell_all()
+        if (game.buy.rect[0] < game.mouse_pos[0] < game.buy.rect[
+            0] + game.buy.rect[2]) and \
+                (game.buy.rect[1] < game.mouse_pos[1] <
+                 game.buy.rect[1] + game.buy.rect[3]):
+            self.is_buy()
 
 
 class Menu:
